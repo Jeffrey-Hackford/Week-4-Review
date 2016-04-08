ccf2552327ed5e97dc3bc3c1719ad59d8fa09c42
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-04-08 13:49:22 -0700

    complete business logic with use of constructor and prototype for calculating cost

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,24 @@ function Pizza (crust, topping1, topping2, topping3) {
   this.topping3 = topping3;
 }
 
+Pizza.prototype.cost = function(newPizza) {
+  return newPizza.crust + newPizza.topping1 + newPizza.topping2 + newPizza.topping3;
+}
 
 $(document).ready(function() {
   $("form#orderForm").submit(function(event) {
     event.preventDefault();
 
-    var newPizza = new Pizza (inputtedCrust, topping1, topping2, topping3);
+    var crust = parseInt($('input[name="crust"]:checked').val());
+    var topping1 = parseInt($("#topping1 :selected").val());
+    var topping2 = parseInt($("#topping2 :selected").val());
+    var topping3 = parseInt($("#topping3 :selected").val());
 
-    var inputtedCrust = $('input[name="crust"]:checked').val();
-    var topping1 = $("#topping1 :selected").val();
-    var topping2 = $("#topping2 :selected").val();
-    var topping3 = $("#topping3 :selected").val();
+    var newPizza = new Pizza (crust, topping1, topping2, topping3);
+
+    var totalPrice = newPizza.cost(newPizza);
 
     console.log(newPizza);
+    console.log(totalPrice);
   });
 });
